50aa2cc340630769cb1d1482a6be7c541313b207
Your Name <you@example.com>
2018-08-25 16:41:42 +0200

    elmentem a posztokat

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-let path = "posts/1";
-let dataToSave = {
-  title: "My first saved blog post",
-  text: "Some hilarious content, which proves how awesome I am."
-};
-fb.ref("users.web1").set(dataToSave);
+
 
 fb.ref("posts").once('value').then(data => {
     let savedPosts = data.val();
+    numberOfPosts = savedPosts.length;
   });
+ 
+  let numberOfPosts;
+
+$('.Submit').on('click', (event) => {
+  let path = "posts/" + numberOfPosts;
+  let dataToSave = {
+    title: $(".titletext").val(),
+    text: $(".contenttext").val()
+  };
+  fb.ref(path).set(dataToSave);
+
+  numberOfPosts = numberOfPosts + 1;
+     
+  let number  =  $(".addpostnumber").html();
+  $(".addpostnumber").html(parseInt(number)+1);
+  
+    
+  
+  })
+  
+//  //let neighbours = ['Eszter', 'Laci', 'Tamas', 'Indi'];
+
+// neighbours.forEach((neighbour, index) => {
+//   console.log (neighbour) 
+//   let li = $("<li>").html(neighbour)
+
+// $("ul").append(li);
+
+
+
+
+// if (neighbour === 'Indi'){
+//   li.css("font-weight", "bold")
+// }
+// });
